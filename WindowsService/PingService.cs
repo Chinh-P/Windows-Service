7f0f2cae8812923db5cf21111d83a215d3addb06
Chinh Phan <chinhplq@gmail.com>
2017-10-26 19:50:05 +0800

    add auto call a website

---
+++ WindowsService/PingService.cs
@@ -0,0 +1,54 @@
+﻿using System.Configuration;
+using System.Net;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace WindowsService
+{
+    public partial class PingService : ServiceBase
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public PingService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Set up a timer to trigger every minute.  
+            Timer timer = new Timer();
+            var interval = ConfigurationManager.AppSettings["interval"];
+            timer.Interval = int.Parse(interval); // 60 seconds  
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+            log.Info("Started");
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // do anything here, for example log something
+             
+            
+            var urls = ConfigurationManager.AppSettings["urls"];
+
+            if (!string.IsNullOrWhiteSpace(urls))
+            {
+                var urlList = urls.Split(',');
+                using (var client = new WebClient())
+                {
+                    foreach (var url in urlList)
+                    {
+                        log.Info(url);
+                        client.DownloadData(url);
+                    }
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ WindowsService/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase[] servicesToRun = new ServiceBase[]
+                {
+                    new PingService(),
+                };
+                ServiceBase.Run(servicesToRun);
+            }
+
+
+        }
+
+
+    }
+}
